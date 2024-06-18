d63a322215f9f6ab1c7e187443d6f3636db7c63a
PotentialStyx <62217716+PotentialStyx@users.noreply.github.com>
2024-06-18 15:01:55 -0700

    feat(server): Add simple server to log incoming requests

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::{io, net::UdpSocket};
+
+use parser::{BytesBuf, Message, Parsable};
+
 mod parser;
 
-fn main() {
-    println!("Server coming soon:tm:")
+fn main() -> io::Result<()> {
+    {
+        let socket = UdpSocket::bind("127.0.0.1:8080")?;
+
+        loop {
+            let mut buf = [0; 1024];
+
+            let _ = socket.recv_from(&mut buf)?;
+
+            if buf.is_empty() {
+                continue;
+            }
+
+            let msg = Message::parse(&mut BytesBuf::new(buf.into())).unwrap();
+            println!("{msg:#?}");
+        }
+    }
 }
