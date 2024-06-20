6fa3c53958c38331e8f5c8f4dda256bcff71ebbe
PotentialStyx <62217716+PotentialStyx@users.noreply.github.com>
2024-06-20 14:02:23 -0700

    feat(client): Initial arg parsing impl

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,76 @@
+use clap::Parser;
+
+macro_rules! from_str_enum {
+    (
+        $vis:vis enum $name:ident[$unknown:ident] {
+            $($field:ident,)*
+        }
+    ) => {
+        #[derive(Debug, Clone, PartialEq, Eq)]
+        #[allow(clippy::upper_case_acronyms)]
+        $vis enum $name {
+            $($field,)*
+            $unknown(String)
+        }
+
+        impl From<String> for $name {
+            fn from(s: String) -> Self {
+                match s.as_str() {
+                    $(stringify!($field) => Self::$field,)*
+                    _ => Self::$unknown(s),
+                }
+            }
+        }
+    };
+}
+
+from_str_enum! {
+    enum ArgRecordType[Unknown] {
+        A,
+        NS,
+        AAAA,
+        CNAME,
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    domain: String,
+
+    // #[arg(value_enum)]
+    record_type: Option<ArgRecordType>,
+
+    #[clap(
+        long = "tcp",
+        conflicts_with = "udp",
+        conflicts_with = "tls",
+        conflicts_with = "https"
+    )]
+    tcp: bool,
+    #[clap(
+        long = "udp",
+        conflicts_with = "tcp",
+        conflicts_with = "tls",
+        conflicts_with = "https"
+    )]
+    udp: bool,
+    #[clap(
+        long = "tls",
+        conflicts_with = "udp",
+        conflicts_with = "tcp",
+        conflicts_with = "https"
+    )]
+    tls: bool,
+    #[clap(
+        long = "https",
+        conflicts_with = "tcp",
+        conflicts_with = "tls",
+        conflicts_with = "udp"
+    )]
+    https: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = dbg!(Cli::parse());
 }
